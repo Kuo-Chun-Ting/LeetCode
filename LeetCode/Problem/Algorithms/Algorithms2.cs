b05467f28cdb25a31e794399410db69e1fbf19a9
Lillard <halila@me.com>
2019-08-16 12:06:43 +0800

    加入專案檔案。

---
+++ LeetCode/Problem/Algorithms/Algorithms2.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problem.Algorithms
+{
+    public class Algorithms2
+    {
+        /// <summary>
+        /// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+        /// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+        /// </summary>
+        /// <param name="l1"></param>
+        /// <param name="l2"></param>
+        /// <returns></returns>
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var curr1 = l1;
+            var curr2 = l2;
+            var plus = false;
+
+            ListNode sum = null;
+            ListNode curr = null;
+
+            while (curr1 != null || curr2 != null || plus)
+            {
+                int digit = 0;
+
+                if (plus)
+                {
+                    digit++;
+                }
+
+                if (curr1 != null)
+                {
+                    digit += curr1.val;
+
+                    curr1 = curr1.next;
+                }
+
+                if (curr2 != null)
+                {
+                    digit += curr2.val;
+
+                    curr2 = curr2.next;
+                }
+
+                if (digit >= 10)
+                {
+                    digit -= 10;
+
+                    plus = true;
+                }
+                else
+                {
+                    plus = false;
+                }
+
+                if (sum == null)
+                {
+                    sum = new ListNode(digit);
+                    curr = sum;
+                }
+                else
+                {
+                    curr.next = new ListNode(digit);
+                    curr = curr.next;
+                }
+            }
+
+            return sum;
+        }
+
+        /// <summary>
+        /// 回傳10的N次方
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        private int NSquareOf10(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return NSquareOf10(n - 1) * 10;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Definition for singly-linked list
+    /// </summary>
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+}
---
+++ LeetCode/Program.cs
@@ -0,0 +1,43 @@
+﻿using LeetCode.Problem.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Algo.1
+
+            //var algo1 = new Algorithms1();
+            //var result = algo1.TwoSum(new int[] { 1, 7, 11, 15, 8 }, 9);
+
+            #endregion
+
+            #region Algo.2
+
+            var node1 = new ListNode(2);
+            node1.next = new ListNode(4);
+            node1.next.next = new ListNode(3);
+
+            var node2 = new ListNode(5);
+            node2.next = new ListNode(6);
+            node2.next.next = new ListNode(4);
+
+            var algo2 = new Algorithms2();
+            var result = algo2.AddTwoNumbers(node1, node2); 
+
+            #endregion
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
